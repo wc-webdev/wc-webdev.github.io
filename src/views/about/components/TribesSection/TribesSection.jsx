6089d3e20a683bbcba83e515aa49e5b9ede93dd5
Allan <allan.crisostomo@whitecloak.com>
2019-03-29 16:06:40 +0800

    Update Tribes layout

--- src/views/about/components/TribesSection/TribesSection.jsx
+++ src/views/about/components/TribesSection/TribesSection.jsx
@@ -19,6 +19,7 @@ const Background = styled.div`
   ${Section}:nth-child(2n) > & {
     background-color: var(--color-fg);
   }
+  color: var(--color-bg);
 `
 
 const Foreground = styled.div`
@@ -60,6 +61,14 @@ const SectionDivision = styled.div`
       padding-bottom: 4rem;
     }
   `}
+  ${media.greaterThan('large')`
+    width: 0;
+    flex: auto;
+    padding-top: 2rem;
+    &:last-child {
+      padding-bottom: 2rem;
+    }
+  `}
 `
 
 const SectionContent = styled.div`
@@ -75,6 +84,14 @@ const SectionContent = styled.div`
     display: flex;
     ${SectionDivision}:nth-child(2n) > ${SectionDivisionForeground} > & {
       width: 50%;
+    }
+  `}
+
+  ${media.greaterThan('large')`
+    margin-left: 0;
+    ${SectionDivision}:nth-child(2n) > ${SectionDivisionForeground} > & {
+      margin-left: auto;
+      margin-right: 0;
     }
   `}
 `
@@ -95,9 +112,13 @@ const SectionDivisionBackground = styled.div`
   ${media.greaterThan('medium')`
     display: flex;
   `}
+  
+  ${media.greaterThan('large')`
+    display: none;
+  `}
 `
 
-const SectionLogoContainer = styled.div`
+const SectionDivisionLogoContainer = styled.div`
   font-size: 45rem;
   opacity: 0.25;
   line-height: 1;
@@ -126,128 +147,163 @@ const SubsectionLogo = styled.span`
   font-size: 2em;
 `
 
+const SectionDivisionWrapper = styled.div`
+  
+`
+
+const SectionDivisionContainer = styled.div`
+  ${media.greaterThan('large')`
+    width: 80%;
+    display: flex;
+    align-items: center;
+    margin: 0 auto;
+  `}
+`
+
+const SectionLogoContainer = styled.div`
+  font-size: 55rem;
+  opacity: 0.25;
+  line-height: 1;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  display: none;
+  ${media.greaterThan('large')`
+    display: flex;
+  `}
+`
+
 function TribesSection(props) {
   return (
     <Section
       {...props}
     >
-      <Background />
+      <Background>
+        <SectionLogoContainer>
+          <Logo1 />
+        </SectionLogoContainer>
+      </Background>
       <Foreground>
-        <SectionDivision>
-          <SectionDivisionBackground>
-            <SectionLogoContainer>
-              <Logo1 />
-            </SectionLogoContainer>
-          </SectionDivisionBackground>
-          <SectionDivisionForeground>
-            <SectionContent>
-              <div>
-                <SectionTitle>
-                  Tribes
-                </SectionTitle>
-                <Subsection>
-                  <SubsectionTitle>
-                    <SubsectionLogo>
-                      <TribeLogo
-                        tribe="water"
-                      />
-                    </SubsectionLogo>
-                    Water
-                  </SubsectionTitle>
-                  <p>
-                    Water flows through rivers, from the small streams to the mighty oceans blah blah blah blah, fire is the
-                    cousin of water blah blah blah
-                  </p>
-                  <p>
-                    Expected to have knowledge on web-based cross platform technology
-                    Electron, Ionic, Cordova, React Native, Vue Native
-                  </p>
-                </Subsection>
-                <Subsection>
-                  <SubsectionTitle>
-                    <SubsectionLogo>
-                      <TribeLogo
-                        tribe="air"
-                      />
-                    </SubsectionLogo>
-                    Air
-                  </SubsectionTitle>
-                  <p>
-                    The air we breathe is everywhere blah blah blah
-                  </p>
-                  <p>
-                    Members of this tribe are knowledgeable with the fundamentals of Web development.
-                    HTML, CSS, HTTP concepts
-                  </p>
-                </Subsection>
-              </div>
-            </SectionContent>
-          </SectionDivisionForeground>
-        </SectionDivision>
-        <SectionDivision>
-          <SectionDivisionBackground>
-            <SectionLogoContainer>
-              <Logo1 />
-            </SectionLogoContainer>
-          </SectionDivisionBackground>
-          <SectionDivisionForeground>
-            <SectionContent>
-              <div>
-                <Subsection>
-                  <SubsectionTitle>
-                    <SubsectionLogo>
-                      <TribeLogo
-                        tribe="earth"
-                      />
-                    </SubsectionLogo>
-                    Earth
-                  </SubsectionTitle>
-                  <p>
-                    As stable as the ground where the tallest mountains stand on blah blah blah
-                  </p>
-                  <p>
-                    Expected to have knowledge on serverside technologies
-                    Laravel, WordPress, Drupal, Express
-                  </p>
-                </Subsection>
-                <Subsection>
-                  <SubsectionTitle>
-                    <SubsectionLogo>
-                      <TribeLogo
-                        tribe="metal"
-                      />
-                    </SubsectionLogo>
-                    Metal
-                  </SubsectionTitle>
-                  <p>
-                    The greatest swords are forged from finest metals blah blah blah
-                  </p>
-                  <p>
-                    Members of this subdiscipline composes of elite forces of project setup and web development best practices.
-                    Webpack, Yeoman, Plop, Grunt, Gulp, Rollup
-                  </p>
-                </Subsection>
-                <Subsection>
-                  <SubsectionTitle>
-                    <SubsectionLogo>
-                      <TribeLogo
-                        tribe="fire"
-                      />
-                    </SubsectionLogo>
-                    Fire
-                  </SubsectionTitle>
-                  <p>
-                    Watch how the tongues of fire turn everything into ash; put them in torches and they light up the way
-                  </p>
-                  <p>
-                    Members of this tribe are knowledgeable with development of single-page apps (SPAs).
-                    React, Vue, Angular
-                  </p>
-                </Subsection>
-              </div>
-            </SectionContent>
-          </SectionDivisionForeground>
-        </SectionDivision>
+        <SectionDivisionWrapper>
+          <SectionDivisionContainer>
+            <SectionDivision>
+              <SectionDivisionBackground>
+                <SectionDivisionLogoContainer>
+                  <Logo1 />
+                </SectionDivisionLogoContainer>
+              </SectionDivisionBackground>
+              <SectionDivisionForeground>
+                <SectionContent>
+                  <div>
+                    <SectionTitle>
+                      Tribes
+                    </SectionTitle>
+                    <Subsection>
+                      <SubsectionTitle>
+                        <SubsectionLogo>
+                          <TribeLogo
+                            tribe="water"
+                          />
+                        </SubsectionLogo>
+                        Water
+                      </SubsectionTitle>
+                      <p>
+                        Water flows through rivers, from the small streams to the mighty oceans blah blah blah blah, fire is the
+                        cousin of water blah blah blah
+                      </p>
+                      <p>
+                        Expected to have knowledge on web-based cross platform technology
+                        Electron, Ionic, Cordova, React Native, Vue Native
+                      </p>
+                    </Subsection>
+                    <Subsection>
+                      <SubsectionTitle>
+                        <SubsectionLogo>
+                          <TribeLogo
+                            tribe="air"
+                          />
+                        </SubsectionLogo>
+                        Air
+                      </SubsectionTitle>
+                      <p>
+                        The air we breathe is everywhere blah blah blah
+                      </p>
+                      <p>
+                        Members of this tribe are knowledgeable with the fundamentals of Web development.
+                        HTML, CSS, HTTP concepts
+                      </p>
+                    </Subsection>
+                  </div>
+                </SectionContent>
+              </SectionDivisionForeground>
+            </SectionDivision>
+            <SectionDivision>
+              <SectionDivisionBackground>
+                <SectionDivisionLogoContainer>
+                  <Logo1 />
+                </SectionDivisionLogoContainer>
+              </SectionDivisionBackground>
+              <SectionDivisionForeground>
+                <SectionContent>
+                  <div>
+                    <Subsection>
+                      <SubsectionTitle>
+                        <SubsectionLogo>
+                          <TribeLogo
+                            tribe="earth"
+                          />
+                        </SubsectionLogo>
+                        Earth
+                      </SubsectionTitle>
+                      <p>
+                        As stable as the ground where the tallest mountains stand on blah blah blah
+                      </p>
+                      <p>
+                        Expected to have knowledge on serverside technologies
+                        Laravel, WordPress, Drupal, Express
+                      </p>
+                    </Subsection>
+                    <Subsection>
+                      <SubsectionTitle>
+                        <SubsectionLogo>
+                          <TribeLogo
+                            tribe="metal"
+                          />
+                        </SubsectionLogo>
+                        Metal
+                      </SubsectionTitle>
+                      <p>
+                        The greatest swords are forged from finest metals blah blah blah
+                      </p>
+                      <p>
+                        Members of this subdiscipline composes of elite forces of project setup and web development best practices.
+                        Webpack, Yeoman, Plop, Grunt, Gulp, Rollup
+                      </p>
+                    </Subsection>
+                    <Subsection>
+                      <SubsectionTitle>
+                        <SubsectionLogo>
+                          <TribeLogo
+                            tribe="fire"
+                          />
+                        </SubsectionLogo>
+                        Fire
+                      </SubsectionTitle>
+                      <p>
+                        Watch how the tongues of fire turn everything into ash; put them in torches and they light up the way
+                      </p>
+                      <p>
+                        Members of this tribe are knowledgeable with development of single-page apps (SPAs).
+                        React, Vue, Angular
+                      </p>
+                    </Subsection>
+                  </div>
+                </SectionContent>
+              </SectionDivisionForeground>
+            </SectionDivision>
+          </SectionDivisionContainer>
+        </SectionDivisionWrapper>
       </Foreground>
     </Section>
   )
